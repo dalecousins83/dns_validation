f940a50441e65539d38ddf2772f97d905ae2a478
dalecousins83 <47048091+dalecousins83@users.noreply.github.com>
2025-05-20 19:39:57 +0930

    Update main.py

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ if file_to_check == 0:
 
 
 print("Checking " + str(dir_objects[int(file_to_check) -1]))
-#touch(str(dir_objects[int(file_to_check)-1].replace(".zone","") + "_" + run_type + "-check.txt"))
+
 sourcefile_name = str(dir_objects[int(file_to_check)-1])
 checkfile_name = str(dir_objects[int(file_to_check)-1].replace(".zone","") + "_" + run_type + "-check.txt")
 
